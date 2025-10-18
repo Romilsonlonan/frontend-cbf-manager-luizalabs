d83a2f5e2f958241f31cb5d1459c02799260395e
Romilson Luis <romilsonlonan@gmail.com>
2025-10-18 22:34:21 +0000

    remover o "Search services" de busca na pagina CBF Manager que não esta

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -4,7 +4,6 @@
 import Image from "next/image";
 import {
   Bell,
-  Search,
 } from "lucide-react";
 import {
   DropdownMenu,
@@ -15,7 +14,6 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { PlaceHolderImages } from "@/lib/placeholder-images";
 
@@ -25,18 +23,7 @@ export function DashboardHeader() {
   return (
     <header className="flex h-16 items-center gap-4 border-b bg-card px-4 md:px-6 sticky top-0 z-30">
       <SidebarTrigger className="md:hidden" />
-      <div className="w-full flex-1">
-        <form>
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search services..."
-              className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-            />
-          </div>
-        </form>
-      </div>
+      <div className="w-full flex-1" />
       <Button variant="ghost" size="icon" className="rounded-full">
         <Bell className="h-5 w-5" />
         <span className="sr-only">Toggle notifications</span>
